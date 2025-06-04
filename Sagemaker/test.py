eff6424a441dd2173c2aacbe7c23a7c6f5817d12
Maduka Charles <68832430+MkTitan1@users.noreply.github.com>
2025-06-04 18:09:52 +0100

    Still on generative model

--- Sagemaker/test.py
+++ Sagemaker/test.py
@@ -3,14 +3,14 @@ from data_structs import Vocabulary
 import torch
 
 device = torch.device("mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu")
-vocab = Vocabulary("Voc.txt")
+vocab = Vocabulary("Sagemaker/Voc.txt")
 model = TransformerModel(vocab)
-model.load_state_dict(torch.load("Diffusion-2.ckpt", map_location=device))
+model.load_state_dict(torch.load("Sagemaker/generative-model.ckpt", map_location=device))
 model.eval()
 
 
 with torch.no_grad():
-    output = model.sample(500)
+    output = model.sample(100)
     decoded = [vocab.decode(seq.tolist()) for seq in output]
     for i, seq in enumerate(decoded): print(f"Sample {i+1}: {seq}")
 
