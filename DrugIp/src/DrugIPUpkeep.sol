1c9f480cf038d4a178fdc34dc8b7d4bcf0772626
Godhanded <aezeigwe2003@gmail.com>
2025-06-10 23:52:16 +0200

    fmt contracts

--- DrugIp/src/DrugIPUpkeep.sol
+++ DrugIp/src/DrugIPUpkeep.sol
@@ -2,11 +2,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {AutomationCompatibleInterface} from
+    "@chainlink/contracts/src/v0.8/automation/interfaces/AutomationCompatibleInterface.sol";
+
 interface IDrugIPToken {
     function mintDrugIP(string memory moleculeHash, string memory metadataURI) external returns (uint256);
 }
 
-contract DrugIPUpkeep {
+contract DrugIPUpkeep is AutomationCompatibleInterface {
     struct Candidate {
         string smiles;
         string metadataURI;
@@ -28,15 +31,11 @@ contract DrugIPUpkeep {
     }
 
     function queueMolecule(string memory smiles, string memory metadataURI) public onlyOwner {
-        candidates.push(Candidate({
-            smiles: smiles,
-            metadataURI: metadataURI,
-            processed: false
-        }));
+        candidates.push(Candidate({smiles: smiles, metadataURI: metadataURI, processed: false}));
     }
 
     function checkUpkeep(bytes calldata) external view returns (bool upkeepNeeded, bytes memory performData) {
-        for (uint i = 0; i < candidates.length; i++) {
+        for (uint256 i = 0; i < candidates.length; i++) {
             if (!candidates[i].processed) {
                 return (true, abi.encode(i, candidates[i].smiles, candidates[i].metadataURI));
             }
@@ -45,7 +44,8 @@ contract DrugIPUpkeep {
     }
 
     function performUpkeep(bytes calldata performData) external {
-        (uint index, string memory smiles, string memory metadataURI) = abi.decode(performData, (uint, string, string));
+        (uint256 index, string memory smiles, string memory metadataURI) =
+            abi.decode(performData, (uint256, string, string));
         require(!candidates[index].processed, "Already processed");
 
         drugIPToken.mintDrugIP(smiles, metadataURI);
--- DrugIp/src/DrugIp.sol
+++ DrugIp/src/DrugIp.sol
@@ -3,7 +3,6 @@
 
 // SPDX-License-Identifier: MIT
 
-
 pragma solidity ^0.8.27;
 
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
@@ -11,25 +10,18 @@ import {ERC1155Burnable} from "@openzeppelin/contracts/token/ERC1155/extensions/
 import {ERC1155Supply} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import {ERC1155URIStorage} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
 
-contract DrugIPToken is
-    ERC1155,
-    ERC1155Burnable,
-    ERC1155Supply,
-    ERC1155URIStorage
-{
+contract DrugIPToken is ERC1155, ERC1155Burnable, ERC1155Supply, ERC1155URIStorage {
     uint256 public tokenCounter;
+    address public recieverAddress;
     mapping(bytes32 => bool) public moleculeHasBeenMinted;
-    mapping(bytes32 => uint) public moleculeToTokenId;
+    mapping(bytes32 => uint256) public moleculeToTokenId;
     mapping(address => bool) isOwner;
 
-    event DrugIPMinted(
-        uint256 indexed tokenId,
-        string moleculeHash,
-        string metadataURI
-    );
+    event DrugIPMinted(uint256 indexed tokenId, string moleculeHash, string metadataURI);
 
     constructor(address initialOwner) ERC1155("") {
         isOwner[initialOwner] = true;
+        recieverAddress = initialOwner;
         tokenCounter = 0;
     }
 
@@ -42,23 +34,15 @@ contract DrugIPToken is
         _setURI(newuri);
     }
 
-    function mint(
-        address account,
-        uint256 id,
-        uint256 amount,
-        bytes memory data
-    ) public onlyOwner {
+    function mint(address account, uint256 id, uint256 amount, bytes memory data) public onlyOwner {
         _mint(account, id, amount, data);
     }
 
-    function mintDrugIP(
-        string memory moleculeHash,
-        string memory metadataURI
-    ) external onlyOwner returns (uint256) {
+    function mintDrugIP(string memory moleculeHash, string memory metadataURI) external onlyOwner returns (uint256) {
         bytes32 molHash = keccak256(abi.encodePacked(moleculeHash));
         require(!moleculeHasBeenMinted[molHash], "Molecule already tokenized");
 
-        _mint(address(this), tokenCounter, 1, "");
+        _mint(recieverAddress, tokenCounter, 1, "");
         _setURI(tokenCounter, metadataURI);
 
         moleculeHasBeenMinted[molHash] = true;
@@ -68,33 +52,33 @@ contract DrugIPToken is
         tokenCounter++;
         return tokenCounter - 1;
     }
-    
-    function uri(uint256 tokenId) public view virtual override(ERC1155URIStorage,ERC1155) returns (string memory) {
+
+    function uri(uint256 tokenId) public view virtual override(ERC1155, ERC1155URIStorage) returns (string memory) {
         return super.uri(tokenId);
     }
 
-    function mintBatch(
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) public onlyOwner {
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
         _mintBatch(to, ids, amounts, data);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _update(
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory values
-    ) internal override(ERC1155, ERC1155Supply) {
+    function _update(address from, address to, uint256[] memory ids, uint256[] memory values)
+        internal
+        override(ERC1155, ERC1155Supply)
+    {
         super._update(from, to, ids, values);
     }
 
     function addOwner(address _newOwner) external onlyOwner {
         isOwner[_newOwner] = true;
+    }
+
+    function changReceiver(address _receiver) external onlyOwner {
+        recieverAddress = _receiver;
     }
 }
 
