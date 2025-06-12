2734695be4dad722e6c01b6226899e10744fff9a
Godhanded <aezeigwe2003@gmail.com>
2025-06-12 19:38:24 +0200

    fix: update model path in check_eligibility function for consistency

--- Sagemaker/drug_eligibility.py
+++ Sagemaker/drug_eligibility.py
@@ -67,7 +67,7 @@ def compute_physchem_properties(mol):
 def lipinski_rule(mw, logp, h_donors, h_acceptors):
     return (mw <= 500) and (logp <= 5) and (h_donors <= 5) and (h_acceptors <= 10)
 
-def check_eligibility(smiles, model_path="Sagemaker/toxicity_model.pth"):
+def check_eligibility(smiles, model_path="toxicity_model.pth"):
     mol = Chem.MolFromSmiles(smiles)
     if mol is None:
         raise ValueError("Invalid SMILES string")
